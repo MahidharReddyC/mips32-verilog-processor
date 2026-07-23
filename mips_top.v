2fa317ae47a4d592e384074c51476cc9c84acd4a
MahidharReddyC <mahidharreddyc47@gmail.com>
2026-07-23 16:50:32 +0530

    Update mips_top.v

--- mips_top.v
+++ mips_top.v
@@ -108,7 +108,7 @@ module instr_memory (
     output [31:0] instr
 );
 reg [31:0] mem [0:255];
-initial $readmemh("program.hex", mem);
+initial $readmemh("program_single.hex", mem);
 assign instr = mem[addr[31:2]];
 endmodule
 
